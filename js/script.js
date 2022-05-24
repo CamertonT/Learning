e594d2d72f63b5c00dcb66d4f197b834ee13e86c
CamertonT <paliy.tores@gmail.com>
2022-05-24 15:24:32 +0300

    completed my first homework

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 "use strict";
-let incr = 10,
-    decr = 10;
 
-alert(`our number = 10. Increment - ${++incr}, decrement - ${--decr}`);
+let numberOfFilms = prompt("Hom many films have you watched?", "");
+console.log(`I have watched - ${numberOfFilms} films`);
+
+// task 2
+console.log(`///////////////////`);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+console.log(personalMovieDB);
+console.log(`///////////////////`);
+
+let filmName1 = prompt("One of the latest film?","");
+let ratefilmName1 = prompt("How would you rate it?","");
+let filmName2 = prompt("One of the latest film?","");
+let ratefilmName2 = prompt("How would you rate it?","");
+const movies = {};
+personalMovieDB.movies[filmName1] = ratefilmName1;
+personalMovieDB.movies[filmName2] = ratefilmName2;
+console.log(personalMovieDB);
