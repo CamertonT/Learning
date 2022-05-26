f4fac1bf86748131f4d744d50b4e4d75e4883d95
CamertonT <paliy.tores@gmail.com>
2022-05-26 13:08:46 +0300

    Completed 3-d stage of task

--- js/script.js
+++ js/script.js
@@ -1,45 +1,85 @@
 "use strict";
 
-let numberOfFilms = prompt("Hom many films have you watched?", "");
-console.log(`I have watched - ${numberOfFilms} films`);
+let numberOfFilms;
+
+
+function start () {
+    numberOfFilms = prompt("Hom many films have you watched?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt("Hom many films have you watched?", "");
+    }
+
+}
+
+start();
 
 // task 2
-console.log(`///////////////////`);
+//console.log(`///////////////////`);
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: true
 };
 
-console.log(personalMovieDB);
-console.log(`///////////////////`);
+//console.log(personalMovieDB);
+//console.log(`///////////////////`);
 
 // task 3
 
- for (let i = 0; i < 2; i++) {
-    let filmName = prompt("One of the latest film?", ""),
-        ratefilmName = prompt("How would you rate it?", "");
+function rememberMyFilms () {
+    for (let i = 0; i < 2; i++) {
+        let filmName = prompt("One of the latest film?", ""),
+            ratefilmName = prompt("How would you rate it?", "");
+        
+        if (filmName != null && filmName != '' &&
+             ratefilmName != null && ratefilmName != ''&& filmName.length < 50){
     
-    if (filmName != null && filmName != '' &&
-         ratefilmName != null && ratefilmName != ''&& filmName.length < 50){
-
-        personalMovieDB.movies[filmName] = ratefilmName;
-    } else {
-        i--;
+            personalMovieDB.movies[filmName] = ratefilmName;
+        } else {
+            i--;
+        }
     }
 }
 
-console.log(personalMovieDB);
-console.log(`///////////////////`);
+//rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    alert('You watched not much films');
-} else if (personalMovieDB.count >=10 && personalMovieDB.count <= 30) {
-    alert('You are classic viewer');
-} else if (personalMovieDB.count > 30) {
-    alert('You are kinoman');
-} else {
-    alert('An error occurd');
+//console.log(personalMovieDB);
+//console.log(`///////////////////`);
+
+function detectPersonalLever () {
+    if (personalMovieDB.count < 10) {
+        alert('You watched not much films');
+    } else if (personalMovieDB.count >=10 && personalMovieDB.count <= 30) {
+        alert('You are classic viewer');
+    } else if (personalMovieDB.count > 30) {
+        alert('You are kinoman');
+    } else {
+        alert('An error occurd');
+    }
 }
+
+detectPersonalLever();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(`result of function showMyDB`);
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres () {
+    for(let i=0; i<3; i++) {
+        let answer = prompt(`Write your havorite genres ${i+1}: `,'');
+        while (answer == '' || answer == null) {
+            answer = prompt(`Write your havorite genres ${i+1}: `,'');
+        }
+        personalMovieDB.genres[i]=answer;
+    }
+}
+writeYourGenres();
+console.log(personalMovieDB);
